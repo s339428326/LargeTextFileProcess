50c0057586cdfa14f5f9de423d17dfe24972d8fb
stanley wong <s339428326@gmail.com>
2024-07-28 22:23:13 +0800

    加入專案檔案。

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dotenv.net;
+using dotenv.net.Utilities;
+using System.Collections;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Controller.File.EnvInit(); //設置環境檔案
+            string PROG_ENV = Controller.GetEnvValue("PROG_ENV");
+            Console.WriteLine($"目前執行環境為:{PROG_ENV}");
+
+            if (PROG_ENV == "DEV") Console.ReadKey();
+        }
+
+    }
+}
